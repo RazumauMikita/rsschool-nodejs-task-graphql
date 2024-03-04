e670d1e7fc8a8fe08a8c2ddf98de0aa76e7402f4
Mikita <anykey2solo@gmail.com>
2024-03-04 20:46:13 +0300

    feat: add user type

---
+++ src/routes/graphql/types/userType.ts
@@ -0,0 +1,78 @@
+import {
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLString,
+} from 'graphql';
+import { UUIDType } from './uuid.js';
+import { PrismaClient } from '@prisma/client/extension';
+import { profileType } from './profileType.js';
+import { postType } from './postType.js';
+
+export const userType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(UUIDType),
+      description: 'User ID',
+    },
+    name: {
+      type: new GraphQLNonNull(GraphQLString),
+      description: 'User name',
+    },
+    balance: {
+      type: new GraphQLNonNull(GraphQLFloat),
+      description: 'User balance',
+    },
+
+    profile: {
+      type: profileType,
+      resolve: async ({ id }, _args, context) => {
+        return context.profile.findUnique({ where: { userId: id } });
+      },
+    },
+
+    posts: {
+      type: new GraphQLList(postType),
+      resolve: async ({ id }, _args, context) => {
+        return context.post.findMany({
+          where: {
+            authorId: id,
+          },
+        });
+      },
+    },
+
+    userSubscribedTo: {
+      type: new GraphQLList(userType),
+      resolve: async ({ id }, _args, context: PrismaClient) => {
+        return context.user.findMany({
+          where: {
+            subscribedToUser: {
+              some: {
+                subscriberId: id,
+              },
+            },
+          },
+        });
+      },
+    },
+
+    subscribedToUser: {
+      type: new GraphQLList(userType),
+      resolve: async ({ id }, _args, context: PrismaClient) => {
+        return context.user.findMany({
+          where: {
+            userSubscribedTo: {
+              some: {
+                authorId: id,
+              },
+            },
+          },
+        });
+      },
+    },
+  }),
+});
